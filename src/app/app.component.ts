a735ea1eb401acd51ffda8e78e91ac9aeb0f5c6c
tlkhanh <longkhanhta@gmail.com>
2024-05-14 20:05:45 +0700

    create embed view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-template-deep';
+  @ViewChild('tmpl') tmpl!: TemplateRef<any>;
+  @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
+  show() {
+    this.entry.createEmbeddedView(this.tmpl, {
+      $implicit: 'Kyo',
+      age: 24,
+    });
+  }
 }
